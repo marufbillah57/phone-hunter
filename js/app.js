786b13a18e640d5e5df8b2b9b23ff694c7bd9605
marubillah57 <marufbillah5157@gmail.com>
2022-03-01 03:21:40 +0600

    details property added

--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
 const loadPhone = () => {
-    const searchText = document.getElementById('search-input').value;
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    const searchInput = document.getElementById('search-input');
+    const searchValue = searchInput.value;
+    // clear input value
+    searchInput.value = '';
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
     fetch(url)
         .then(response => response.json())
-        .then(data => showPhoneDetails(data.data));
+        .then(data => displayShowPhone(data.data));
 };
 
-const showPhoneDetails = phones => {
+// Phone show in display
+const displayShowPhone = phones => {
     // console.log(phones);
+    const containerShowPhone = document.getElementById('show-phone');
+    // clear data
+    containerShowPhone.textContent = '';
     for(const phone of phones){
-        console.log(phone);
-        const containerShowPhone = document.getElementById('show-phone-details');
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -19,10 +24,39 @@ const showPhoneDetails = phones => {
                <div class="card-body mt-2 mt-md-4 text-center">
                     <h5 class="card-title">Name: ${phone.phone_name}</h5>
                     <p class="card-text">Brand: ${phone.brand}</p>
-                    <a href="#" class="btn btn-primary">Details</a>
+                    <button id="phone-details" onclick="loadPhoneDetail('${phone.slug}')" class="btn btn-primary">Details</button>
                 </div>
             </div>
         `;
         containerShowPhone.appendChild(div);
+        // console.log(phone);
     }
+};
+
+// Phone details load
+const loadPhoneDetail = id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => showPhoneDetails(data.data));
+};
+
+// Display show phone details
+const showPhoneDetails = phone => {
+    console.log(phone)
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.textContent = '';
+    const div = document.createElement('div');
+    div.className = 'col-6';
+    div.innerHTML = `
+        <div class="card p-3 p-md-4 rounded-3 border-success mb-2 mb-md-4">
+            <img height="150px" width="150px"  src="${phone.image}" class=" img-fluid m-auto" alt="...">
+        <div class="card-body mt-2 mt-md-4 text-center">
+                <h5 class="card-title">Name: ${phone.name}</h5>
+                <p class="card-text">Brand: ${phone.brand}</p>
+                <p class="card-text">Release: ${phone.releaseDate}</p>
+            </div>
+        </div>
+    `;
+    phoneDetails.appendChild(div);
 }
