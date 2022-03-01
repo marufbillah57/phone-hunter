22d2e7fd75449ba5d45811d22cb53908e82be77d
marubillah57 <marufbillah5157@gmail.com>
2022-03-01 13:06:08 +0600

    sensors details added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+
 const loadPhone = () => {
     const searchInput = document.getElementById('search-input');
     const searchValue = searchInput.value;
@@ -11,11 +13,19 @@ const loadPhone = () => {
 
 // Phone show in display
 const displayShowPhone = phones => {
-    // console.log(phones);
+    const first20Phone = phones.slice(0, 20);
+    console.log(first20Phone)
     const containerShowPhone = document.getElementById('show-phone');
     // clear data
     containerShowPhone.textContent = '';
-    for(const phone of phones){
+
+    // error message
+    const errorMessage = document.getElementById('error');
+    if(phones == '' || isNaN(phones) || !phones){
+        errorMessage.style.display = 'block';
+    }
+    first20Phone.forEach(phone => {
+        errorMessage.style.display = 'none';
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -30,7 +40,7 @@ const displayShowPhone = phones => {
         `;
         containerShowPhone.appendChild(div);
         // console.log(phone);
-    }
+    });
 };
 
 // Phone details load
@@ -43,20 +53,34 @@ const loadPhoneDetail = id => {
 
 // Display show phone details
 const showPhoneDetails = phone => {
-    console.log(phone)
+
+    toggleDisplayPhone('none');
     const phoneDetails = document.getElementById('phone-details');
     phoneDetails.textContent = '';
     const div = document.createElement('div');
-    div.className = 'col-6';
+    div.className = 'col-sm-8 col-md-4';
     div.innerHTML = `
-        <div class="card p-3 p-md-4 rounded-3 border-success mb-2 mb-md-4">
+        <div class="card p-3 p-md-4 rounded-3 border-success mb-5 mb-md-5">
             <img height="150px" width="150px"  src="${phone.image}" class=" img-fluid m-auto" alt="...">
         <div class="card-body mt-2 mt-md-4 text-center">
                 <h5 class="card-title">Name: ${phone.name}</h5>
                 <p class="card-text">Brand: ${phone.brand}</p>
-                <p class="card-text">Release: ${phone.releaseDate}</p>
+                <p class="card-text">Release: ${phone.releaseDate ? phone.releaseDate : "Comming Up"}</p>
+                <p class="card-text">Chip: ${phone.mainFeatures.chipSet}</p>
+                <p class="card-text">Memory: ${phone.mainFeatures.memory}</p>
+                <p id="sensors" class="card-text">Sensors: ${phone.mainFeatures.sensors}</p>
             </div>
         </div>
     `;
     phoneDetails.appendChild(div);
+}
+
+// display single phone details
+const toggleDisplayPhone = displayStyle => {
+    document.getElementById('show-phone').style.display = displayStyle;
+}
+
+// display data clear
+const toggleDisplayPhoneDetails = displayStyle => {
+    document.getElementById('phone-details').style.display = displayStyle;
 }
