e57f6894bbe593459e913c2d5bc2a6458901ef8c
marubillah57 <marufbillah5157@gmail.com>
2022-03-01 20:20:50 +0600

    mobile and desktop responsive

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Error Message
+// clear data
 const clearData = phoneId => {
     document.getElementById(phoneId).textContent = '';
 }
@@ -50,18 +50,16 @@ const displayShowPhone = phones => {
 
    const errorMessage1 = document.getElementById('error-msg-1');
    const errorMessage2 = document.getElementById('error-msg-2');
-   if(!phones || searchInput == '') {
-       errorMessage2.innerText = 'Phone no found';
-   } else if(isNaN(phones)){
+   if(isNaN(phones)){
        errorMessage1.innerText = 'Please enter a phone name';
    }
     first20Phone.forEach(phone => {
         document.getElementById('error-msg-1').style.display = 'none';
         document.getElementById('error-msg-2').style.display = 'none';
         const div = document.createElement('div');
-        div.classList.add('col');
+        div.className = ('col-sm-8 col-md-4');
         div.innerHTML = `
-            <div class="card p-3 p-md-4 rounded-3 border-success">
+            <div class="card p-3 p-md-4 rounded-3 border-success mb-3 mb-md-5">
                 <img  src="${phone.image}" class="card-img-top w-50 m-auto" alt="...">
                <div class="card-body mt-2 mt-md-4 text-center">
                     <h5 class="card-title">Name: ${phone.phone_name}</h5>
@@ -85,24 +83,29 @@ const loadPhoneDetail = id => {
 
 // Display show phone details
 const showPhoneDetails = phone => {
+    clearData('show-phone');
     const phoneDetails = document.getElementById('phone-details');
     phoneDetails.textContent = '';
     const div = document.createElement('div');
-    div.className = 'col-sm-8 col-md-4';
+    div.className = 'col-sm-8 col-md-6';
     div.innerHTML = `
-        <div class="card p-3 p-md-4 rounded-3 border-success mb-5 mb-md-5">
-            <img height="150px" width="150px"  src="${phone.image}" class=" img-fluid m-auto" alt="...">
+        <div class="card p-3 rounded-3 border-success mb-5 mb-md-5">
+            <img  src="${phone.image}" class=" img-fluid m-auto" alt="...">
         <div class="card-body mt-2 mt-md-4 text-center">
                 <h5 class="card-title">Name: ${phone.name}</h5>
                 <p class="card-text">Brand: ${phone.brand}</p>
                 <p class="card-text">Release: ${phone.releaseDate ? phone.releaseDate : "Comming Up"}</p>
-                <p class="card-text">Chip: ${phone.mainFeatures.chipSet}</p>
-                <p class="card-text">Memory: ${phone.mainFeatures.memory}</p>
-                <p id="sensors" class="card-text">Sensors: ${phone.mainFeatures.sensors}</p>
+                <p class="card-text">Chip: ${phone.mainFeatures.chipSet ? phone.mainFeatures.chipSet : 'No found'}</p>
+                <p class="card-text">Memory: ${phone.mainFeatures.memory ? phone.mainFeatures.memory : 'No found'}</p>
+                <p class="card-text">Bluetooth: ${phone.others?.Bluetooth ?  phone.others?.Bluetooth : 'No found'}</p>
+                <p class="card-text">GPS: ${phone.others?.GPS ?  phone.others?.GPS : 'No found'}</p>
+                <p class="card-text">WLAN: ${phone.others?.WLAN ?  phone.others?.WLAN : 'No found'}</p>
+                <p id="sensors" class="card-text">Sensors: ${phone.mainFeatures.sensors ? phone.mainFeatures.sensors : 'No found'}</p>
             </div>
         </div>
     `;
     phoneDetails.appendChild(div);
+    console.log(phone)
 }
 
 
